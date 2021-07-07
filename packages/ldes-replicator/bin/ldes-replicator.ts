c5cf98db71e798335990219382f51a5d8ce2131a
D34DPlayer <d34dplayer@protonmail.com>
2021-07-07 15:38:43 +0200

    Add a dotenv for the ldes-replicator

--- packages/ldes-replicator/bin/ldes-replicator.ts
+++ packages/ldes-replicator/bin/ldes-replicator.ts
@@ -6,20 +6,23 @@ import { DummyConnector } from '@ldes/ldes-dummy-connector';
 import { DummyState } from '@ldes/ldes-dummy-state';
 
 import { newEngine } from '@treecg/actor-init-ldes-client';
+import 'dotenv/config';
 import { Orchestrator } from '../lib/Orchestrator';
 
 // TODO: Parse and use CLI parameters
+
+const URL = process.env.URL || 'https://apidg.gent.be/opendata/adlib2eventstream/v1/dmg/objecten';
+const POLL_INTERVAL = Number.parseInt(process.env.pollingInterval || '5000', 10);
 
 async function run(): Promise<void> {
   const connector = new DummyConnector();
   const state = new DummyState();
 
-  const url = 'https://apidg.gent.be/opendata/adlib2eventstream/v1/dmg/objecten';
   const options = {
-    pollingInterval: 5_000,
+    pollingInterval: POLL_INTERVAL,
   };
   const LDESClient = newEngine();
-  const eventstreamSync = LDESClient.createReadStream(url, options);
+  const eventstreamSync = LDESClient.createReadStream(URL, options);
 
   const orchestrator = new Orchestrator([connector], state, eventstreamSync);
 
