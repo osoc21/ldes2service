1d4faa2544723017ebf00c6192b116ebcf71b637
Demian Dekoninck <demian@live.be>
2021-07-08 21:16:56 +0200

    use -r to preload dotenv
    See https://github.com/motdotla/dotenv#preload

--- packages/ldes-replicator/bin/ldes-replicator.ts
+++ packages/ldes-replicator/bin/ldes-replicator.ts
@@ -6,7 +6,6 @@ import { DummyConnector } from '@ldes/ldes-dummy-connector';
 import { DummyState } from '@ldes/ldes-dummy-state';
 
 import { newEngine } from '@treecg/actor-init-ldes-client';
-import 'dotenv/config';
 import { Orchestrator } from '../lib/Orchestrator';
 
 // TODO: Parse and use CLI parameters
