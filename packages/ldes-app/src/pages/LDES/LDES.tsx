e5504b86e3f902ac5596e323793b27317e5ef8e1
Demian Dekoninck <demian@live.be>
2021-07-12 20:08:22 +0200

    add favicon

--- packages/ldes-app/src/pages/LDES/LDES.tsx
+++ packages/ldes-app/src/pages/LDES/LDES.tsx
@@ -4,7 +4,7 @@ import { H1, H2 } from '../../components/Headings';
 export function LDES() {
   return (
     <div>
-      <H1>LDES2Service</H1>
+      <H1>Linked Data Event Streams</H1>
       <div className="mt-6">
         <H2>Coming soon.</H2>
       </div>
